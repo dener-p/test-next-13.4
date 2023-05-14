eecb85e3c2e5d9cb9fb0b7e0bbd2c9d64fc2b1b1
Dener <denerpuhl@gmail.com>
2023-05-14 17:54:33 -0300

    should work

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client"
+export default function Error() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-slate-500">Hello World</h1>
+      <p className="text-slate-500">Error</p>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-export default function Home() {
-  const testAction = async () => {
-    "use server"
-    console.log("test")
-  }
+export default async function Home() {
+  const jsonPlaceholder = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  )
+  // const testAction = async () => {
+  //   "use server"
+  //   console.log("test")
+  // }
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //     <form action={testAction} className="flex flex-col text-slate-500">
+  //       <input type="text" />
+  //       <button type="submit">Submit</button>
+  //     </form>
+  //   </main>
+  // )
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={testAction} className="flex flex-col text-slate-500">
-        <input type="text" />
-        <button type="submit">Submit</button>
-      </form>
+      <h1 className="text-4xl font-bold text-slate-500">Hello World</h1>
+      <p className="text-slate-500">
+        {JSON.stringify(await jsonPlaceholder.json())}
+      </p>
     </main>
   )
 }
